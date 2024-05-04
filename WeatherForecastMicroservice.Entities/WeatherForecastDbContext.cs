bd59985829dccf6706613e67c1508dd6dd0a8da2
Doug Holland <doug.holland@outlook.com>
2024-05-04 16:15:22 -0700

    Updated the Xml comments within the WeatherForecastDbContext class.

--- WeatherForecastMicroservice.Entities/WeatherForecastDbContext.cs
+++ WeatherForecastMicroservice.Entities/WeatherForecastDbContext.cs
@@ -5,20 +5,20 @@
     using WeatherForecastMicroservice.Model;
 
     /// <summary>
-    /// 
+    /// The weather forecast database context.
     /// </summary>
     public class WeatherForecastDbContext : DbContext
     {
         /// <summary>
-        /// 
+        /// Creates an instance of the <see cref="T:WeatherForecastDbContex"/> class.
         /// </summary>
-        /// <param name="options"></param>
+        /// <param name="options">The database context options.</param>
         public WeatherForecastDbContext(DbContextOptions<WeatherForecastDbContext> options) : base(options)
         {
         }
 
         /// <summary>
-        /// 
+        /// Gets or sets the weather forecasts.
         /// </summary>
         public DbSet<WeatherForecast> WeatherForecasts
         {
