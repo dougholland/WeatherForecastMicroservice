0e9c210dbf4b62e7441aff392ce8bf3d609aab8b
Doug Holland <doug.holland@outlook.com>
2024-05-04 18:41:18 -0700

    Updated the ErrorControllerUnitTest to mock the use of the ErrorController class within a production environment.

--- WeatherForecastMicroservice.UnitTests/ErrorControllerUnitTests.cs
+++ WeatherForecastMicroservice.UnitTests/ErrorControllerUnitTests.cs
@@ -26,12 +26,12 @@
         /// <summary>
         /// The mock logger instance to be used for unit tests.
         /// </summary>
-        private readonly ILogger<ErrorController> logger = new Mock<ILogger<ErrorController>>().Object;
+        private readonly Mock<ILogger<ErrorController>> logger = new Mock<ILogger<ErrorController>>();
 
         /// <summary>
         /// The mock environment to be used for unit tests.
         /// </summary>
-        private readonly IHostEnvironment environment = new Mock<IHostEnvironment>().Object;
+        private readonly Mock<IHostEnvironment> environment = new Mock<IHostEnvironment>();
 
         /// <summary>
         /// Provides initialization for unit tests.
@@ -48,7 +48,7 @@
         [TestMethod]
         public void HandleError()
         {
-            var controller = new ErrorController(this.logger);
+            var controller = new ErrorController(this.logger.Object);
 
             controller.ControllerContext = new ControllerContext
             {
@@ -66,16 +66,36 @@
         [TestMethod]
         public void HandleErrorDevelopment()
         {
-            var controller = new ErrorController(this.logger);
+            var controller = new ErrorController(this.logger.Object);
 
             controller.ControllerContext = new ControllerContext
             {
                 HttpContext = this.httpContext ?? new DefaultHttpContext()
             };
 
-            var result = controller.HandleErrorDevelopment(environment);
+            var result = controller.HandleErrorDevelopment(environment.Object);
 
             Assert.IsNotNull(result);
+        }
+
+        /// <summary>
+        /// Tests the <see cref="M:ErrorController.HandleErrorDevelopment"/> method, mocking its use within a production environment.
+        /// </summary>
+        [TestMethod]
+        public void HandleErrorDevelopment_EnvironmentIsNotDevelopment()
+        {
+            var controller = new ErrorController(this.logger.Object);
+
+            controller.ControllerContext = new ControllerContext
+            {
+                HttpContext = this.httpContext ?? new DefaultHttpContext()
+            };
+
+            environment.Setup(e => e.EnvironmentName).Returns("Production");
+
+            var result = controller.HandleErrorDevelopment(environment.Object);
+
+            Assert.IsInstanceOfType(result, typeof(NotFoundResult));
         }
     }
 }
