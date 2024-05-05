0da4b6dc4272980cc670a043c4d5c502e10417bc
Doug Holland <doug.holland@outlook.com>
2024-05-04 21:59:36 -0700

    Updated the WeatherForecastDbContextFactoryUnitTests class.

---
+++ WeatherForecastMicroservice.UnitTests/WeatherForecastDbContextFactoryUnitTests.cs
@@ -0,0 +1,34 @@
+﻿namespace WeatherForecastMicroservice.UnitTests
+{
+    using Microsoft.Extensions.Configuration;
+
+    using WeatherForecastMicroservice.Entities;
+
+    /// <summary>
+    /// Provides unit tests for the <see cref="T:WeatherForecastDbContextFactory"/> class.
+    /// </summary>
+    [TestClass]
+    public class WeatherForecastDbContextFactoryUnitTests
+    {
+        /// <summary>
+        /// Provides initialization for unit tests.
+        /// </summary>
+        [TestInitialize]
+        public void Initialize()
+        {            
+        }
+
+        /// <summary>
+        /// Tests the <see cref="M:WeatherForecastDbContextFactory.CreateDbContext"/> method.
+        /// </summary>
+        [TestMethod]
+        public void WeatherForecastDbContextFactory()
+        {
+            var factory = new WeatherForecastDbContextFactory();
+
+            var context = factory.CreateDbContext();
+
+            Assert.IsNotNull(context);
+        }
+    }
+}
