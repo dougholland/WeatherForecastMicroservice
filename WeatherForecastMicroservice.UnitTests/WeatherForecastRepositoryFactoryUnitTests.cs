e187e622555c9ba3c7a53e9f71874f702eed71e4
Doug Holland <doug.holland@outlook.com>
2024-05-07 10:29:10 -0700

    Created a new WeatherForecastRepositoryFactoryUnitTests class.

---
+++ WeatherForecastMicroservice.UnitTests/WeatherForecastRepositoryFactoryUnitTests.cs
@@ -0,0 +1,24 @@
+﻿namespace WeatherForecastMicroservice.UnitTests
+{
+    using WeatherForecastMicroservice.Entities;
+
+    /// <summary>
+    /// Provides unit tests for the <see cref="T:WeatherForecastRepositoryFactory"/> class.
+    /// </summary>
+    [TestClass]
+    public class WeatherForecastRepositoryFactoryUnitTests
+    {
+        /// <summary>
+        /// Tests the <see cref="M:WeatherForecastRepositoryFactory.CreateRepository"/> method.
+        /// </summary>
+        [TestMethod]
+        public void WeatherForecastDbContextFactory()
+        {
+            var factory = new WeatherForecastRepositoryFactory();
+
+            var repository = factory.CreateRepository();
+
+            Assert.IsNotNull(repository);
+        }
+    }
+}
